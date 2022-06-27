9e69800bbd84c6462e4ec8639d82ec8f8fc07e9f
AlexanderPodovinnikov <art.iq@hotmail.com>
2022-06-28 00:00:47 +0300

    Initial Commit

---
+++ YourPiano/YourPianoApp.swift
@@ -0,0 +1,17 @@
+//
+//  YourPianoApp.swift
+//  YourPiano
+//
+//  Created by Alex Po on 28.06.2022.
+//
+
+import SwiftUI
+
+@main
+struct YourPianoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
