646e1f39a894a6cb38031426587f7b0c7c12c49d
AlexanderPodovinnikov <art.iq@hotmail.com>
2022-06-29 14:23:16 +0300

    Data Model, draft home view and 2 additional tabs for open and finished sections

--- YourPiano/ContentView.swift
+++ YourPiano/ContentView.swift
@@ -8,14 +8,34 @@
 import SwiftUI
 
 struct ContentView: View {
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        TabView {
+            HomeView()
+                .tabItem {
+                    Image(systemName: "house")
+                    Text("Home")
+                }
+            ProjectsView(showClosedProjects: false)
+                .tabItem {
+                    Image(systemName: "list.bullet")
+                    Text("In progress")
+                }
+            ProjectsView(showClosedProjects: true)
+                .tabItem {
+                    Image(systemName: "checkmark")
+                    Text("Finished")
+                }
+        }
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
+    static var dataController = DataController.preview
+
     static var previews: some View {
         ContentView()
+            .environment(\.managedObjectContext, dataController.container.viewContext)
+            .environmentObject(dataController)
     }
 }
---
+++ YourPiano/HomeView.swift
@@ -0,0 +1,28 @@
+//
+//  HomeView.swift
+//  YourPiano
+//
+//  Created by Alex Po on 28.06.2022.
+//
+
+import SwiftUI
+
+struct HomeView: View {
+    @EnvironmentObject var dataController: DataController
+        
+    var body: some View {
+        VStack {
+            Button("Add Data") {
+                dataController.deleteAll()
+                try? dataController.createSampleData()
+            }
+        }
+        .navigationTitle("Home")
+    }
+}
+
+struct HomeView_Previews: PreviewProvider {
+    static var previews: some View {
+        HomeView()
+    }
+}
