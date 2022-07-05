9d99fb17a95ed7fc8e0bd8002d7a4c3478d98f67
AlexanderPodovinnikov <art.iq@hotmail.com>
2022-07-05 18:00:45 +0300

    Redesigned Home View

---
+++ YourPiano/Color-Additions.swift
@@ -0,0 +1,13 @@
+//
+//  Color-Additions.swift
+//  YourPiano
+//
+//  Created by Alex Po on 04.07.2022.
+//
+
+import SwiftUI
+
+extension Color {
+    static let systemGroupedBackground = Color(UIColor.systemGroupedBackground)
+    static let secondarySystemGroupedBackground = Color(UIColor.secondarySystemGroupedBackground)
+}
